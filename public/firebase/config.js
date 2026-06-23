aaf19f3a7e67bc032cb83abab4d8b1ba67a55b3a
Lucas Alves <lucasbambam@hotmail.com>
2026-06-23 10:01:52 -0300

    Modelo de estrutura no aplicativo

---
+++ public/firebase/config.js
@@ -0,0 +1,37 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged }
+  from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import {
+  getFirestore, doc, setDoc, getDoc, deleteDoc,
+  collection, getDocs, writeBatch,
+  onSnapshot, query, orderBy, serverTimestamp, limit
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { getStorage, ref as storageRef, uploadBytes, getDownloadURL, deleteObject }
+  from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAMu4ViHLd0FnDb37RJcokY0ZRL9o5Y13s",
+  authDomain: "escalacao-fc.firebaseapp.com",
+  projectId: "escalacao-fc",
+  storageBucket: "escalacao-fc.firebasestorage.app",
+  messagingSenderId: "565171747971",
+  appId: "1:565171747971:web:67dc73d4d270eb68b1efb6",
+  measurementId: "G-TVLYCVCQD8"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+const provider = new GoogleAuthProvider();
+
+// Expose to global scope for Babel script
+window.__firebase = {
+  auth, db, storage, provider,
+  signInWithPopup, signOut, onAuthStateChanged,
+  doc, setDoc, getDoc, deleteDoc,
+  collection, getDocs, writeBatch,
+  onSnapshot, query, orderBy, serverTimestamp, limit,
+  storageRef, uploadBytes, getDownloadURL, deleteObject
+};
+window.dispatchEvent(new Event("firebase-ready"));
